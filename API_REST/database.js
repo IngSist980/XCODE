c210cc9186ff455dd01133458e4f9928164c4e9e
Ryan <123036783+IngSist980@users.noreply.github.com>
2026-01-24 22:10:11 -0600

    Varios cambios

--- API_REST/database.js
+++ API_REST/database.js
@@ -1,11 +1,11 @@
 // Configuración de la conexión a PostgreSQL
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '12345',
-    port: 5432
-  });
-  
-  module.exports = pool;
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'WenDani808402$$',
+  port: 5433
+});
+
+module.exports = pool;
